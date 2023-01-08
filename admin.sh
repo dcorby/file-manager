25530795abf9054d485ceb0c640282bd31c41f52
Your Name <you@example.com>
2023-01-07 19:07:37 -0500

    working on filesystem app

---
+++ admin.sh
@@ -0,0 +1,26 @@
+echo "rm ~/.android/avd/Pixel_XL_API_33.avd/*.lock"
+echo "mksdcard -l mySdCard 1024M mySdCardFile.img"
+echo "export PATH=/home/dmc7z/Android/Sdk/emulator:\$PATH"
+echo "adb shell"
+echo "sudo adb push 52096f49 /sdcard/Android/data/com.example.media/"
+echo "emulator -avd Pixel_XL_API_33"
+
+
+# SQLLite
+# https://stackoverflow.com/questions/18370219/how-to-use-adb-in-android-studio-to-view-an-sqlite-db
+echo "SQLLite"
+echo "adb shell"
+echo "su (first thing you do)"
+echo "run-as com.example.media"
+echo "cd data/data/com.example.media/databases/"
+echo "sqlite3 <your-db-name>.db"
+echo ".tables"
+echo ".schema tablename"
+
+echo "lsof -n -i:5037"
+echo "lsof -i:5554"
+
+dmc7z@dmc7z-Yoga-6-13ALC6:~/AndroidStudioProjects/FileSystem$ kill -9 125114
+dmc7z@dmc7z-Yoga-6-13ALC6:~/AndroidStudioProjects/FileSystem$ adb kill-server
+dmc7z@dmc7z-Yoga-6-13ALC6:~/AndroidStudioProjects/FileSystem$ adb start-server
+
--- app/src/main/java/com/example/filesystem/MainActivity.kt
+++ app/src/main/java/com/example/filesystem/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.filesystem
 
+import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import android.provider.DocumentsContract
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import com.example.filesystem.data.MyFile
-import com.example.filesystem.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.filesystem.data.MyFile
+import com.example.filesystem.databinding.ActivityMainBinding
+
 
 const val MYFILE_ID = "myfile id"
 
@@ -38,18 +44,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setSupportActionBar(binding.toolbar)
-
-        //val navController = findNavController(R.id.nav_host_fragment_content_main)
-        //appBarConfiguration = AppBarConfiguration(navController.graph)
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-
-        //binding.fab.setOnClickListener { view ->
-        //    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //        .setAnchorView(R.id.fab)
-        //        .setAction("Action", null).show()
-        //}
-
         /* Instantiates headerAdapter and filesAdapter. Both adapters are added to concatAdapter.
         which displays the contents sequentially */
         val headerAdapter = HeaderAdapter()
@@ -69,6 +63,84 @@ class MainActivity : AppCompatActivity() {
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener {
             fabOnClick()
+        }
+
+        /* get full permissions for external storage from user */
+        // https://developer.android.com/reference/android/provider/Settings#ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+//        val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+//        startActivity(
+//            Intent(
+//                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+//                uri
+//            )
+//        )
+
+//        var canAccessExternalStorage: Boolean = false
+//        if (android.os.Build.VERSION.SDK_INT >= 30) {
+//            if (Environment.isExternalStorageManager()) {
+//                canAccessExternalStorage = true
+//            }
+//        } else {
+//            canAccessExternalStorage = true
+//        }
+//
+//        if (canAccessExternalStorage) {
+//
+//        }
+
+        /* allow the user to pick a directory subtree for storage */
+        // https://commonsware.com/community/t/is-simple-case-of-creating-multiple-files-and-giving-the-user-an-easy-access-to-them-is-impossible-with-stored-access-framework/630
+        // http://android-er.blogspot.com/2015/09/example-of-using-intentactionopendocume.html
+        //val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        // content://com.android.externalstorage.documents/tree/primary%3ADocuments
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker when it loads.
+            // "You can only use an uri that you obtained before with ACTION_OPEN_DOCUMENT"
+            val pickerInitialUri: Uri = "/Documents".toUri()
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, "content://com.android.externalstorage.documents/tree/primary:Documents")
+            //putExtra(DocumentsContract.EXTRA_INITIAL_URI, "Documents")
+        }
+        val pickerInitialUri: Uri = "/Documents".toUri()
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, "content://com.android.externalstorage.documents/tree/primary:Documents")
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, "Documents")
+        resultLauncher.launch(intent)
+    }
+
+    // "Persist Permissions" - so user doesn't have to select everytime
+    // "To preserve access to files across device restarts and create a better user experience, your app can "take" the persistable URI permission grant that the system offers, as shown in the following code snippet:"
+    // https://developer.android.com/training/data-storage/shared/documents-files#persist-permissions
+
+    /* ActivityResult class for pick directory subtree */
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // this works and prompts user to select a folder
+            val data: Intent? = result.data
+            val uri: Uri? = data?.data
+            Log.v("ZZZ", uri.toString())
+            val childrenUri = DocumentsContract.buildChildDocumentsUriUsingTree(
+                uri,
+                DocumentsContract.getTreeDocumentId(uri)
+            )
+
+            var fields = arrayOf<String>(DocumentsContract.Document.COLUMN_DOCUMENT_ID, DocumentsContract.Document.COLUMN_DISPLAY_NAME, DocumentsContract.Document.COLUMN_MIME_TYPE)
+            var cursor = contentResolver.query(childrenUri, fields, null, null, null)
+
+            // directions to get all files recursively
+            // add folder, e.g. Documents/FileSystem, or allow user to
+            // https://stackoverflow.com/questions/64408944/documentscontracts-buildchilddocumentsuriusingtreeuri-documentscontract-gettr
+            while (cursor!!.moveToNext()) {
+                var docId = cursor.getString(0)
+                var name = cursor.getString(1)
+                var mime = cursor.getString(2)
+                Log.v("ZZZ", "docId: " + docId + ", name: " + name + ", mime: " + mime)
+                // docId: primary:Documents/bar.txt, name: bar.txt, mime: text/plain
+                //if (isDirectory(mime)) {
+                //    final Uri newNode = DocumentsContract.buildChildDocumentsUriUsingTree(rootUri, docId);
+                //    dirNodes.add(newNode);
+                //}
+            }
+
         }
     }
 
