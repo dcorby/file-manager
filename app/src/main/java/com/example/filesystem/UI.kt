b70b99c370c7270959c3fe1daa572be40a981d61
Your Name <you@example.com>
2023-02-06 21:00:24 -0500

    Handling copy and move button is active on backstack

--- app/src/main/java/com/example/filesystem/UI.kt
+++ app/src/main/java/com/example/filesystem/UI.kt
@@ -22,6 +22,8 @@ class UI {
                 val sourceFragmentDocId = receiver.getActionState("copy", "sourceFragmentDocId")!!
                 val sourceDocId = receiver.getActionState("copy", "sourceDocId")!!
                 showStatus(binding.status, "Copying", sourceFragmentDocId, sourceDocId, "copy")
+            } else {
+                binding.toggleGroup.uncheck(R.id.action_copy)
             }
         }
 
@@ -31,6 +33,8 @@ class UI {
                 val sourceFragmentDocId = receiver.getActionState("move", "sourceFragmentDocId")!!
                 val sourceDocId = receiver.getActionState("move", "sourceDocId")!!
                 showStatus(binding.status, "Moving", sourceFragmentDocId, sourceDocId, "move")
+            } else {
+                binding.toggleGroup.uncheck(R.id.action_move)
             }
         }
 
